7b82c649520b2799e7a3ea2bd37c4e84a15cc2d2
tttom3669 <tttom3669@gmail.com>
2025-02-23 15:18:08 +0800

    套件新增

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/worldWear_2025_hex_react/',
+  base: '/worldWear_2024_hex_react/',
   plugins: [react()],
 })
