0c69e68c462f9e64b067f5a10182cd1211256c1c
tttom3669 <tttom3669@gmail.com>
2025-02-23 17:40:53 +0800

    update: 圖片處理

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   base:
-    process.env.NODE_ENV === 'production' ? '' : '/',
+    process.env.NODE_ENV === 'production' ? '/worldWear_2024_hex_react/' : '/',
   plugins: [react()],
 });
